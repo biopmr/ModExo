137d0da309a9af367d1967cceacc148ab68c3303
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2017-05-12 12:15:59 -0300

    void serial event removed

--- arduino_control/main.ino
+++ arduino_control/main.ino
@@ -131,20 +131,6 @@ void setup() {
   //END TIMER SETUP
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-  }
-}
-
 //***************
 // CAN STARTUP
 //***************
