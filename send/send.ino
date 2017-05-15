5fb2125974f13eff0d0e7b486a9254a428e64cfe
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2017-05-15 13:38:34 -0300

    code cleaning and preparing for merge

--- send/send.ino
+++ send/send.ino
@@ -5,12 +5,14 @@
 
 #include <SPI.h>
 #include "mcp_can.h"
+#include <mcp_can_dfs.h>
 
 
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 9;
-uint32_t data = 0;
+uint32_t encoder_data = 0;
+uint32_t loadcell_data = 0;
 
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
@@ -39,45 +41,38 @@ void loop()
 
     unsigned int canId = CAN.getCanId();
 
-    Serial.println("-----------------------------");
-    Serial.print("Get data from ID: ");
-    Serial.println(canId, HEX);
-
-    for (int i = 0; i < len; i++) // print the data
-    {
-      Serial.print(buf[i], HEX);
-      Serial.print("\t");
-    }
-    Serial.println();
-
     // encoder information is read from buf[4] and buf[5] and converted to decimal
-    enconder_data = buf[4];
-    enconder_data <<= 8;
-    enconder_data = data | buf[5];
+    encoder_data = buf[4];
+    encoder_data <<= 8;
+    encoder_data = encoder_data | buf[5];
 
-    // load cell information is read from buf[1], buf[2] and buf[3] and converted to decimal
-    loadcell_data = buf[1];
-    loadcell_data <<= 8;
-    loadcell_data = data | buf[2];
-    loadcell_data <<= 8;
-    loadcell_data = data | buf[3];
+    // // load cell information is read from buf[1], buf[2] and buf[3] and converted to decimal
+    // loadcell_data = buf[1];
+    // loadcell_data <<= 8;
+    // loadcell_data = loadcell_data | buf[2];
+    // loadcell_data <<= 8;
+    // loadcell_data = loadcell_data | buf[3];
 
-    if (buf[1] >= 128) {
-           laodcell_data = loadcell_data - 16777216;
-    }
+    // if (buf[1] >= 128) {
+    //        loadcell_data = loadcell_data - 16777216;
+    // }
     
-    Serial.print("Encoder: ");
-    Serial.println(encoder_data, DEC);
+       Serial.print("Encoder: ");
+       Serial.println(encoder_data, DEC);
 
-    Serial.print("Loadcell: ");
-    Serial.println(laodcell_data, DEC);
+    // Serial.print("Loadcell: ");
+    // Serial.println(loadcell_data/1000, DEC);
 
-    delay(400);
+    // delay(400);
   }
 
 }
 
 
+
 /*********************************************************************************************************
   END FILE
 *********************************************************************************************************/
+
+
+
