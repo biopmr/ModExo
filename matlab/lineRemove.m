70e55794722503f6ea168db88677c0fd43bc6663
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2017-08-18 17:43:22 -0300

    added function that delete array lines

---
+++ matlab/lineRemove.m
@@ -0,0 +1,15 @@
+
+initial=1;
+final=2;
+%
+ for i = 1:(final-initial+1)
+   Encoder(initial,:) = [];
+   EPOSCurrent(initial,:) = [];
+   EPOSPosition(initial,:) = [];
+   LoadCell(initial,:) = [];
+   SampleNumber10000samplesp(initial,:) = [];
+   Timems(initial,:) = [];
+   X1(initial,:) = [];
+   X2(initial,:) = [];
+   X3(initial,:) = [];
+ end
